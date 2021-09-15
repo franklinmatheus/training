53835b0e9e975b831f444c020e91d574f127f63b
franklinmatheus <franklinmatheus1@gmail.com>
2021-09-14 23:49:23 -0300

    mais um

--- .githooks/check-msg.py
+++ .githooks/check-msg.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
+import sys, os, re
+from subprocess import check_output
+
+branch = check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip()
+print("commit-msg: On branch '%s'" % branch)
+
+url = "https://api.github.com/repos/franklinmatheus/training/issues"
+
 print("OK")
+
