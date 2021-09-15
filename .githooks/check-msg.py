861324ebf978c5d7b2a9c852713f18b6bf3e0d53
franklinmatheus <franklinmatheus1@gmail.com>
2021-09-14 23:58:58 -0300

    6

--- .githooks/check-msg.py
+++ .githooks/check-msg.py
@@ -7,6 +7,15 @@ branch = check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip()
 print(branch)
 
 url = "https://api.github.com/repos/franklinmatheus/training/issues"
+commit_msg_filepath = sys.argv[0]
+print(sys.argv)
+
+with open(commit_msg_filepath, 'r') as f:
+    content = f.read()
+    if not content.startswith():
+        #print("commit-msg: ERROR! The commit message must start with")
+        #sys.exit(1)
+        print(content)
 
 print("OK")
 
